3b76094d1e6fc40c425269b8c949b8857be0a771
vsvipul11 <58623632+vsvipul11@users.noreply.github.com>
2025-01-21 19:05:34 +0530

    Create config.ts

---
+++ lib/config.ts
@@ -0,0 +1,5 @@
+// lib/config.ts
+export const config = {
+  liveKitUrl: process.env.NEXT_PUBLIC_LIVEKIT_URL || 'https://fixie-test-73wt3hzv.livekit.cloud',
+  fixieUrl: process.env.NEXT_PUBLIC_FIXIE_URL || 'wss://prod-voice-pgaenaxiea-uc.a.run.app',
+};
